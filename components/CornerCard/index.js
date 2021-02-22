5cc1eda3a905029d2e66b3b7850026f87f52432b
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2021-02-21 21:09:03 -0300

    first page style DONE

---
+++ components/CornerCard/index.js
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+function CornerCard({ isRed }) {
+  return (
+    <div className={`${isRed ? "bg-red-400" : "bg-blue-400"} w-auto h-auto flex flex-col items-center justify-center p-10 rounded-3xl shadow-md`}>
+      <h2 className='font-bebas text-5xl text-black'>RED CORNER</h2>
+      <div className='mt-10'>
+        <Image
+          src={isRed ? '/git-red.svg' : '/git-blue.svg'}
+          width={62}
+          height={62}
+        />
+      </div>
+      <p className={`${isRed ? 'text-red-900' : 'text-blue-900'} font-bebas text-2xl  text-center mt-5`}>
+        Enter the contender
+        <br />
+        username
+      </p>
+      <input
+        type='text'
+        className={` ${isRed ? 'border-red-900' : 'border-blue-900'} outline-none border-b-2  bg-transparent font-bebas text-black text-center text-2xl p-1 mt-5`}
+      />
+    </div>
+  );
+}
+
+export default CornerCard;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,49 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from 'next/image';
+import CornerCard from '../components/CornerCard';
+import Tilt from 'react-parallax-tilt';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className='flex flex-col items-center min-h-screen py-2 bg-white'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Octo Battle</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className='w-full max-w-screen-2xl min-h-screen flex flex-col items-center'>
+        <div className='mt-10 md:mt-20'>
+          <Tilt glareEnable>
+            <Image
+              src='/octobattle-logo.svg'
+              width={300}
+              height={300}
+              layout='intrinsic'
+            />
+          </Tilt>
+          <p className='font-bebas text-center '>
+            Created by{' '}
+            <a
+              className='underline transition-all hover:text-red-600'
+              target='_blank'
+              href='http://lfverissimo.com'
+            >
+              LF Verissimo
+            </a>
+          </p>
+        </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-red-400 focus:text-green-400"
-          >
-            <h3 className="text-3xl font-bold">Documentation &rarr;</h3>
-            <p className="mt- text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className='w-full flex justify-center items-center mt-10 flex-col md:flex-row '>
+          <CornerCard isRed />
+          <div className='w-auto flex flex-col items-center p-20'>
+            <h3 className='font-bebas text-7xl text-red-600'>VS.</h3>
+            <button className='bg-black font-bebas text-4xl text-white py-4 px-10 rounded-3xl mt-5 transition-all flex items-center justify-center hover:bg-red-600 transform hover:-translate-y-2'>
+              FIGHT!
+            </button>
+          </div>
+          <CornerCard />
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
