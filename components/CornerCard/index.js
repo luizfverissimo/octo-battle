820fb9f0905c732707e3c3edc9414322498467f4
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2021-02-27 11:35:12 -0300

    motion ADDED

--- components/CornerCard/index.js
+++ components/CornerCard/index.js
@@ -1,8 +1,39 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 function CornerCard({ isRed }) {
+  const variantsLeftFadeIn = {
+    visible: {
+      opacity: 1,
+      x: 0
+    },
+    hidden: {
+      opacity: 0,
+      x: -30
+    }
+  };
+
+  const variantsRightFadeIn = {
+    visible: {
+      opacity: 1,
+      x: 0
+    },
+    hidden: {
+      opacity: 0,
+      x: 30
+    }
+  };
+
   return (
-    <div className={`${isRed ? "bg-red-400" : "bg-blue-400"} w-auto h-auto flex flex-col items-center justify-center p-10 rounded-3xl shadow-md transition-all transform hover:scale-105 hover:shadow-lg`}>
+    <motion.div
+      variants={isRed ? variantsLeftFadeIn : variantsRightFadeIn}
+      initial='hidden'
+      animate='visible'
+      transition={{ duration: 1 }}
+      className={`${
+        isRed ? 'bg-red-400' : 'bg-blue-400'
+      } w-auto h-auto flex flex-col items-center justify-center p-10 rounded-3xl shadow-md transition-all transform hover:scale-105 hover:shadow-lg`}
+    >
       <h2 className='font-bebas text-5xl text-black'>RED CORNER</h2>
       <div className='mt-10 transform transition-all hover:-translate-y-2'>
         <Image
@@ -11,16 +42,22 @@ function CornerCard({ isRed }) {
           height={62}
         />
       </div>
-      <p className={`${isRed ? 'text-red-900' : 'text-blue-900'} font-bebas text-2xl  text-center mt-5`}>
+      <p
+        className={`${
+          isRed ? 'text-red-900' : 'text-blue-900'
+        } font-bebas text-2xl  text-center mt-5`}
+      >
         Enter the contender
         <br />
         username
       </p>
       <input
         type='text'
-        className={` ${isRed ? 'border-red-900' : 'border-blue-900'} outline-none border-b-2  bg-transparent font-bebas text-black text-center text-2xl p-1 mt-5`}
+        className={` ${
+          isRed ? 'border-red-900' : 'border-blue-900'
+        } outline-none border-b-2  bg-transparent font-bebas text-black text-center text-2xl p-1 mt-5`}
       />
-    </div>
+    </motion.div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,31 @@ import Head from 'next/head';
 import Image from 'next/image';
 import CornerCard from '../components/CornerCard';
 import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion';
 
 export default function Home() {
+  const variantsTopFadeIn = {
+    visible: {
+      opacity: 1,
+      y: 0
+    },
+    hidden: {
+      opacity: 0,
+      y: -50
+    }
+  };
+
+  const variantsBottomFadeIn = {
+    visible: {
+      opacity: 1,
+      y: 0
+    },
+    hidden: {
+      opacity: 0,
+      y: 50
+    }
+  };
+
   return (
     <div className='flex flex-col items-center min-h-screen py-2 bg-white'>
       <Head>
@@ -12,7 +35,13 @@ export default function Home() {
       </Head>
 
       <main className='w-full max-w-screen-2xl min-h-screen flex flex-col items-center'>
-        <div className='mt-10 md:mt-20'>
+        <motion.div
+          className='mt-10 md:mt-20'
+          variants={variantsTopFadeIn}
+          initial='hidden'
+          animate='visible'
+          transition={{ duration: 1 }}
+        >
           <Tilt>
             <Image
               src='/octobattle-logo.svg'
@@ -31,16 +60,22 @@ export default function Home() {
               LF Verissimo
             </a>
           </p>
-        </div>
+        </motion.div>
 
         <div className='w-full flex justify-center items-center mt-10 flex-col md:flex-row '>
           <CornerCard isRed />
-          <div className='w-auto flex flex-col items-center p-20'>
+          <motion.div
+            variants={variantsBottomFadeIn}
+            initial='hidden'
+            animate='visible'
+            transition={{ duration: 1, delay: .5 }}
+            className='w-auto flex flex-col items-center p-20'
+          >
             <h3 className='font-bebas text-7xl text-red-600'>VS.</h3>
             <button className='bg-black font-bebas text-4xl text-white py-4 px-10 rounded-3xl mt-5 transition-all flex items-center justify-center hover:bg-red-600 transform hover:-translate-y-2'>
               FIGHT!
             </button>
-          </div>
+          </motion.div>
           <CornerCard />
         </div>
       </main>
